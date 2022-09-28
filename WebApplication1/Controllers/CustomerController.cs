4612d93f447deaacdc2a6722e27c15eb2bc65103
mscian <67492958+mscian@users.noreply.github.com>
2022-09-28 18:29:16 -0300

    add

---
+++ WebApplication1/Controllers/CustomerController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomerController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Gabriela", "Juan", "Ramiro", "Martin"
+        };
+
+        private readonly ILogger<CustomerController> _logger;
+
+        public CustomerController(ILogger<CustomerController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Customer> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Customer
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
